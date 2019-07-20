8ee46146588b20f26e535dd1da19a6ec9d6b9dd0
BlogBD <zengweiwymail@163.com>
2019-07-20 21:27:28 +0800

    update

--- MYSTORE/src/main/java/cn/store/demo/web/filter/EncodingFilter.java
+++ MYSTORE/src/main/java/cn/store/demo/web/filter/EncodingFilter.java
@@ -1,25 +1,21 @@
-package cn.store.demo.utils;
+package cn.store.demo.web.filter;
 
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
-import javax.servlet.http.HttpServletResponse;
 /**
  * 统一编码
  *
  * @author Administrator
  */
+@WebFilter(filterName = "EncodingFilter" ,urlPatterns = "/*")
 public class EncodingFilter implements Filter {
-
   @Override
   public void init(FilterConfig filterConfig) throws ServletException {
     // TODO Auto-generated method stub
