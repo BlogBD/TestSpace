c256c2fbcd4406f44ed875cfa067615e41788b7a
BlogBD <zengweiwymail@163.com>
2019-08-04 09:49:58 +0800

    update

--- Hibernate/src/main/java/com/hibernate/Demo02/HibernateDemo02.java
+++ Hibernate/src/main/java/com/hibernate/Demo02/HibernateDemo02.java
@@ -2,8 +2,10 @@ package com.hibernate.Demo02;
 
 import com.hibernate.Demo01.mapping.Customer;
 import com.hibernate.utils.HibernateUtils;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 import org.hibernate.query.Query;
 import org.junit.Test;
 
@@ -36,7 +38,39 @@ public class HibernateDemo02 {
     transaction.commit();
   }
 
-    /**
-     * 分页查询
-     */
+  /** 分页查询 */
+  @Test
+  public void test03() {
+    Session currentSession = HibernateUtils.getCurrentSession();
+    Transaction transaction = currentSession.beginTransaction();
+    // 设置分页
+    Query query = currentSession.createQuery("from Customer");
+    query.setFirstResult(1);
+    query.setMaxResults(2);
+    List<Customer> list = query.list();
+    list.forEach(System.out::println);
+    transaction.commit();
+  }
+
+  /**
+   * Criteria的使用
+   */
+  @Test
+  public void test04() {
+    Session currentSession = HibernateUtils.getCurrentSession();
+    Transaction transaction = currentSession.beginTransaction();
+    Criteria criteria = currentSession.createCriteria(Customer.class);
+    // 查询全部
+    // criteria.list().forEach(i -> System.out.println((Customer)i));
+
+    // 模糊查询
+    //criteria.add(Restrictions.like("custName", "王%")).list().forEach(i -> System.out.println((Customer)i));
+
+    //分页
+    criteria.setFirstResult(1);
+    criteria.setMaxResults(2);
+    criteria.list().forEach(i -> System.out.println((Customer)i));
+
+    transaction.commit();
+  }
 }
